de47765416bf828105031e04db08d772f7840566
Yiğit Sağlam <ysaglam602@gmail.com>
2026-07-13 00:04:14 +0300

    Initial Commit

---
+++ Newsys/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Newsys
+//
+//  Created by Yiğit Sağlam on 12.07.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NewsysUITests/NewsysUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  NewsysUITestsLaunchTests.swift
+//  NewsysUITests
+//
+//  Created by Yiğit Sağlam on 12.07.2026.
+//
+
+import XCTest
+
+final class NewsysUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
